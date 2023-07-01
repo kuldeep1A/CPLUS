20e6277342f934228a96f6d60043bc7ef3913473
KULDEEP <kuldeepdhangarkd@gmail.com>
2023-07-01 13:08:38 +0530

    access value through the iterator and also access the key value pair in map using iterator with different way

---
+++ learn/iterator/iterator.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> values{1, 2, 3, 4, 5, 6};
+
+    for (vector<int>::iterator it = values.begin(); it != values.end(); it++)
+    {
+        cout << *it << " ";
+    }
+
+    using ScoreMap = unordered_map<string, int>;
+    // using ScoreMapConstIter = ScoreMap::const_iterator;
+    ScoreMap map;
+    map["one"] = 1;
+    map["two"] = 2;
+    map["three"] = 3;
+    map["four"] = 4;
+    cout << endl;
+
+    cout << "first Way " << endl;
+
+    for (ScoreMap::const_iterator it = map.begin(); it != map.end(); it++)
+    {
+        auto &key = it->first;
+        auto &value = it->second;
+
+        cout << key << " = " << value << endl;
+    }
+
+    cout << "second Way " << endl;
+    for (auto keyVal : map)
+    {
+        auto &key = keyVal.first;
+        auto &value = keyVal.second;
+
+        cout << key << " = " << value << endl;
+    }
+
+    cout << "Third Way " << endl;
+    for (auto [key, value] : map)
+    {
+        cout << key << " = " << value << endl;
+    }
+}
