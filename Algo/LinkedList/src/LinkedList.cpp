2eefd55e71c3ab1fef85236f6757ff43f3ee378a
KULDEEP <kuldeepdhangarkd@gmail.com>
2023-07-27 20:46:24 +0530

    start linkedlist

---
+++ Algo/LinkedList/src/LinkedList.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+    int data;
+    Node *next;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = nullptr;
+        size++;
+    }
+};
+
+class LinkedList
+{
+public:
+    LinkedList()
+    {
+        Head = nullptr;
+    }
+}
+
+int main
+{
+}
