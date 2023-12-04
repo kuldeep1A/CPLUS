2fb28e19f6982e9f19795a902dd423345b8e5f04
KULDEEP DHANGAR <kuldeepdhangarkd@gmail.com>
2023-12-04 15:37:31 +0530

    Circular Queue Implementation*

--- Algo/Queue/Circular Queue/circular_queue.cpp
+++ Algo/Queue/Circular Queue/circular_queue.cpp
@@ -6,27 +6,33 @@ const int MAX_SIZE = 5; // Maximum size of the circluar queue
 int queue[MAX_SIZE];
 int front = -1, rear = -1; // Initialize front and rear indices
 
-
 // Function to check if the queue is empty
-bool isEmpty() {
+bool isEmpty()
+{
     return front == -1 && rear == -1;
 }
 
 // Function to check if the queue is full
-bool isFull() {
+bool isFull()
+{
     return (rear + 1) % MAX_SIZE == front;
 }
 
 // Function to enqueue (insert) an item
-void enqueue(int item) {
-    if (isFull()) {
+void enqueue(int item)
+{
+    if (isFull())
+    {
         cout << "Queue is full." << endl;
         return;
     }
 
-    if (isEmpty()){
+    if (isEmpty())
+    {
         front = rear = 0; // Set both front and rear to 0 for the first item
-    } else {
+    }
+    else
+    {
         rear = (rear + 1) % MAX_SIZE;
     }
 
@@ -35,27 +41,33 @@ void enqueue(int item) {
 }
 
 // Function to dequeue (remove) an item
-int dequeue() {
-    if (isEmpty()) {
+int dequeue()
+{
+    if (isEmpty())
+    {
         cout << "Queue is empty" << endl;
         return -1; // Return an error value
     }
 
     int item = queue[front]; // Retrieve item at front
 
-    if (front == rear) {   // If only one item in the queue
+    if (front == rear)
+    {                      // If only one item in the queue
         front = rear = -1; // Reset to empty state
-    } else {
+    }
+    else
+    {
         front = (front + 1) % MAX_SIZE; // Circular increment of front
     }
 
     return item;
 }
 
-
-// Function to peek (get the ffront item without removing) 
-int peek(){
-    if (isEmpty()) {
+// Function to peek (get the ffront item without removing)
+int peek()
+{
+    if (isEmpty())
+    {
         cout << "Queue is empty" << endl;
         return -1; // Return an error value
     }
@@ -63,8 +75,8 @@ int peek(){
     return queue[front];
 }
 
-
-int main() {
+int main()
+{
     // Ex. usage
     enqueue(10);
     enqueue(20);
@@ -76,5 +88,3 @@ int main() {
     enqueue(100);
     enqueue(1000);
 }
-
-
